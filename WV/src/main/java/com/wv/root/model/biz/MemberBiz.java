c1d571ca7e8cb211b94814f9dfab8c2702ed65c9
mun tae yeon <monty420420@naver.com>
2020-12-24 21:52:53 +0900

    m333
    m333

--- WV/src/main/java/com/wv/root/model/biz/MemberBiz.java
+++ WV/src/main/java/com/wv/root/model/biz/MemberBiz.java
@@ -1,10 +1,8 @@
 package com.wv.root.model.biz;
 
-import org.springframework.stereotype.Service;
 
 import com.wv.root.model.dto.MemberDto;
 
-@Service
 public interface MemberBiz {
 	public MemberDto login(MemberDto dto);
     public int insert(MemberDto dto);
