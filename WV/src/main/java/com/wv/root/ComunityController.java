d0ad551527d08d55de090391ec9a6013103f94f4
sarsdin <sarsdin@gmail.com>
2021-01-15 05:42:08 +0900

    1/15 jm#1
    검색,카테고리,페이징

--- WV/src/main/java/com/wv/root/ComunityController.java
+++ WV/src/main/java/com/wv/root/ComunityController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.wv.root.model.biz.ComunityBiz;
 import com.wv.root.model.dto.ComunityDto;
 import com.wv.root.model.dto.CpDto;
+import com.wv.root.model.dto.CpDto.Search;
 
 @Controller
 public class ComunityController {
@@ -26,56 +27,44 @@ public class ComunityController {
 	@Autowired
 	private ComunityBiz combiz;
 	
-/*	@RequestMapping(value = "", method = RequestMethod.GET)
-	public String comunityMain(Model model, String category) {
-		logger.info("[comunity main]");
+//	@RequestMapping(value= "comunity1.do", method=RequestMethod.GET)
+//	public String cmPageChange(Model model, CpDto oldcpdto, @ModelAttribute("schdto") CpDto.Search schdto) {
+//		logger.info("[comunity main]");						//초기에 category, currentpage포함
+//		List<ComunityDto> list = null;
+//		CpDto cpdto = null;
+//		
+//		cpdto = new CpDto(combiz.countList(oldcpdto), oldcpdto.getCurrentPage());
+//		cpdto.setCategory(oldcpdto.getCategory());
+//		
+//		list = combiz.selectAll(cpdto);
+//		System.out.println(list.size());
+//		System.out.println(cpdto);
+//		
+//		model.addAttribute("cpdto", cpdto);
+//		model.addAttribute("list", list);
+//		
+//		return "comunity";
+//	}
+	
+	@RequestMapping(value = "comunity.do", method = RequestMethod.GET)
+	public String comunityMain(Model model, CpDto dto, @ModelAttribute("schdto") Search schdto) {
+		logger.info("[comunity Main]");
 		List<ComunityDto> list = null;
-		if(category!=null) {
-//			list = combiz.selectAll(category);
-		} else {
-			category = "전체";
-//			list = combiz.selectAll(category);
-		}
-		System.out.println("test 카테고리값 >>> "+category);
-		System.out.println("test list값 >>> "+list);
+		dto.setSearch(schdto); //전달하면 dto 쓰임 끝 
+		System.out.println("Test sch--->"+ dto.getSearch());
 		
-		CpDto cpdto = new CpDto(list.size(), 1);
-		cpdto.setCategory(category);
-		System.out.println("test cpdto값 >>> "+cpdto);
+		CpDto cpdto = new CpDto(combiz.countList(dto), dto);
+		cpdto.setSearch(schdto); //위의 dto와 다르므로 다시 받아야함 
+		System.out.println("Test cpdto값 >>> "+cpdto);
+		System.out.println("Test schdto값 >>> "+schdto);
 		
-		model.addAttribute("cpdto", cpdto);
+		list = combiz.selectAll(cpdto);
+		model.addAttribute("cpdto", cpdto );
 		model.addAttribute("list", list );
 
 		return "comunity";
 	}
-	*/
-	@RequestMapping(value= "comunity.do", method=RequestMethod.GET)
-	public String cmPageChange(Model model, @ModelAttribute CpDto oldcpdto) {
-		logger.info("[comunity main]");						//초기에 category, currentpage포함
-		List<ComunityDto> list = null;
-		CpDto cpdto = null;
-		
-//		if(oldcpdto.getCategory() != null) {
-//			cpdto = new CpDto(combiz.selectAll(oldcpdto).size(), oldcpdto.getCurrentPage());
-//			list = combiz.selectAll(cpdto);
-//		} else {
-//			oldcpdto.setCategory("전체");
-//			cpdto = new CpDto(combiz.selectAll(oldcpdto).size(), oldcpdto.getCurrentPage());
-//			list = combiz.selectAll(cpdto);
-//		}
-		
-		cpdto = new CpDto(combiz.countList(oldcpdto), oldcpdto.getCurrentPage());
-		cpdto.setCategory(oldcpdto.getCategory());
-		
-		list = combiz.selectAll(cpdto);
-		System.out.println(list.size());
-		System.out.println(cpdto);
-		
-		model.addAttribute("cpdto", cpdto);
-		model.addAttribute("list", list);
-		
-		return "comunity";
-	}
+	
 	
 
 	@RequestMapping(value = "comsidemenu.do", method = RequestMethod.POST)
--- WV/src/main/java/com/wv/root/model/dto/CpDto.java
+++ WV/src/main/java/com/wv/root/model/dto/CpDto.java
@@ -12,7 +12,9 @@ public class CpDto {
 	private int last;	
 	private int prev;	//이전page
 	private int next;	//이후page
-	private String category;	
+	private String category;
+	private Search search;
+	
 	
 	public CpDto() {
 		super();
@@ -29,17 +31,32 @@ public class CpDto {
 		this.prev = first-1;
 		this.next = last+1;
 	}
-	
-	//paging시 새로운 페이지정보를 받아서 전체 데이터를 갱신
-	public void cpdtoChg(int totalData, int currentPage) {
+	public CpDto(int totalData, CpDto cpdto) {
+		super();
 		this.totalData = totalData;
 		this.totalPage = (int)(Math.ceil((double)totalData/dataPerPage));
-		this.currentPage = currentPage;
+		this.currentPage = cpdto.getCurrentPage();
 		this.pageGroup = (int)(Math.ceil((double)currentPage/pagePerscreen));
 		this.last = pageGroup*pagePerscreen;
 		this.first = (last-pagePerscreen)+1;
 		this.prev = first-1;
 		this.next = last+1;
+		this.category = cpdto.getCategory();
+		this.search = cpdto.getSearch();
+	}
+	
+	//paging시 새로운 페이지정보를 받아서 전체 데이터를 갱신
+	public void cpdtoChg(int totalData, CpDto cpdto) {
+		this.totalData = totalData;
+		this.totalPage = (int)(Math.ceil((double)totalData/dataPerPage));
+		this.currentPage = cpdto.getCurrentPage();
+		this.pageGroup = (int)(Math.ceil((double)currentPage/pagePerscreen));
+		this.last = pageGroup*pagePerscreen;
+		this.first = (last-pagePerscreen)+1;
+		this.prev = first-1;
+		this.next = last+1;
+		this.category = cpdto.getCategory();
+		this.search = cpdto.getSearch();
 	}
 	
 
@@ -130,19 +147,53 @@ public class CpDto {
 	public void setCategory(String category) {
 		this.category = category;
 	}
+	public Search getSearch() {
+		return search;
+	}
+	public void setSearch(Search search) {
+		this.search = search;
+	}
 
 	@Override
 	public String toString() {
 		return "CpDto [totalData=" + totalData + ", dataPerPage=" + dataPerPage + ", pagePerscreen=" + pagePerscreen
 				+ ", totalPage=" + totalPage + ", currentPage=" + currentPage + ", pageGroup=" + pageGroup + ", first="
-				+ first + ", last=" + last + ", prev=" + prev + ", next=" + next + ", category=" + category + "]";
+				+ first + ", last=" + last + ", prev=" + prev + ", next=" + next + ", category=" + category
+				+ ", search=" + search + "]";
 	}
 
 
-	
-	
-	
-	
+	public static class Search {
+		private String searchsel = "subject";
+		private String word;
+		
+		public Search() {
+			super();
+		}
+		public Search(String searchsel, String word) {
+			super();
+			this.searchsel = searchsel;
+			this.word = word;
+		}
+		public String getSearchsel() {
+			return searchsel;
+		}
+		public void setSearchsel(String searchsel) {
+			this.searchsel = searchsel;
+		}
+		public String getWord() {
+			return word;
+		}
+		public void setWord(String word) {
+			this.word = word;
+		}
+		@Override
+		public String toString() {
+			return "Search [searchsel=" + searchsel + ", word=" + word + "]";
+		}
+
+		
+	}
 
 	
 	
