ec74cff768d2fb178f7017755a4ea98c20e07017
dkfltm <dkfltm0907@naver.com>
2021-02-01 19:14:22 +0900

    contest 2

--- WV/src/main/java/com/wv/root/model/dao/ContestDao.java
+++ WV/src/main/java/com/wv/root/model/dao/ContestDao.java
@@ -3,6 +3,7 @@ package com.wv.root.model.dao;
 import java.util.List;
 
 import com.wv.root.model.dto.ContestDto;
+import com.wv.root.model.util.PagingVO;
 
 public interface ContestDao {
 
@@ -12,6 +13,26 @@ public interface ContestDao {
 
 	public ContestDto detailpage(int contestnum);
 
-	public List<ContestDto> contestList();
+	public List<ContestDto> selectContest(PagingVO vo);
+
+	public List<ContestDto> fieldcontestList(PagingVO vo);
+
+	public List<ContestDto> targetcontestList(PagingVO vo);
+
+	public List<ContestDto> companycontestList(PagingVO vo);
+
+	public List<ContestDto> rewardcontestList(PagingVO vo);
+
+	public int countContest();
+
+	public int countField(String category);
+
+	public int countTarget(String category);
+
+	public int countCompany(String category);
+
+	public int countReward(String category);
+
+	
 	
 }
--- WV/src/main/java/com/wv/root/model/dao/ContestDaoImpl.java
+++ WV/src/main/java/com/wv/root/model/dao/ContestDaoImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.wv.root.model.dto.ContestDto;
+import com.wv.root.model.util.PagingVO;
 
 @Repository
 public class ContestDaoImpl implements ContestDao{
@@ -40,17 +41,123 @@ public class ContestDaoImpl implements ContestDao{
 	}
 
 	@Override
-	public List<ContestDto> contestList() {
+	public List<ContestDto> selectContest(PagingVO vo) {
 		List<ContestDto> list = new ArrayList<ContestDto>();
-		
 		try {
-			list = sqlSession.selectList(NAMESPACE+"selectList");
+			list = sqlSession.selectList(NAMESPACE+"selectcontest", vo);
 		} catch (Exception e) {
-			System.out.println("[error]: select list");
+			System.out.println("[error]: selectcontest");
 			e.printStackTrace();
 		}
-		
 		return list;
+	}
+
+	@Override
+	public List<ContestDto> fieldcontestList(PagingVO vo) {
+		List<ContestDto> list = new ArrayList<ContestDto>();
+		try {
+			list = sqlSession.selectList(NAMESPACE+"fieldList", vo);
+		} catch (Exception e) {
+			System.out.println("[error]: fieldList");
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	@Override
+	public List<ContestDto> targetcontestList(PagingVO vo) {
+		List<ContestDto> list = new ArrayList<ContestDto>();
+		try {
+			list = sqlSession.selectList(NAMESPACE+"targetList", vo);
+		} catch (Exception e) {
+			System.out.println("[error]: targetListlist");
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	@Override
+	public List<ContestDto> companycontestList(PagingVO vo) {
+		List<ContestDto> list = new ArrayList<ContestDto>();
+		try {
+			list = sqlSession.selectList(NAMESPACE+"companyList", vo);
+		} catch (Exception e) {
+			System.out.println("[error]: companylist");
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	@Override
+	public List<ContestDto> rewardcontestList(PagingVO vo) {
+		List<ContestDto> list = new ArrayList<ContestDto>();
+		try {
+			list = sqlSession.selectList(NAMESPACE+"rewardList", vo);
+		} catch (Exception e) {
+			System.out.println("[error]: rewardlist");
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	@Override
+	public int countContest() {
+		int res = 0;
+		try {
+			res = sqlSession.selectOne(NAMESPACE+"countcontest");
+		} catch (Exception e) {
+			System.out.println("[error]: countcontest");
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+	@Override
+	public int countField(String category) {
+		int res = 0;
+		try {
+			res = sqlSession.selectOne(NAMESPACE+"countfield", category);
+		} catch (Exception e) {
+			System.out.println("[error]: countfield");
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+	@Override
+	public int countTarget(String category) {
+		int res = 0;
+		try {
+			res = sqlSession.selectOne(NAMESPACE+"counttarget", category);
+		} catch (Exception e) {
+			System.out.println("[error]: counttarget");
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+	@Override
+	public int countCompany(String category) {
+		int res = 0;
+		try {
+			res = sqlSession.selectOne(NAMESPACE+"countcompany", category);
+		} catch (Exception e) {
+			System.out.println("[error]: countcompany");
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+	@Override
+	public int countReward(String category) {
+		int res = 0;
+		try {
+			res = sqlSession.selectOne(NAMESPACE+"countreward", category);
+		} catch (Exception e) {
+			System.out.println("[error]: countreward");
+			e.printStackTrace();
+		}
+		return res;
 	}
 
 }
